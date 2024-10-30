f1a18ca96ae23fe6816a0541ea05ad3b2f19806d
OmarBknd <omar.bknd@gmail.com>
2024-10-30 03:55:39 +0100

    added a component for the education form

---
+++ src/components/Education.jsx
@@ -0,0 +1,43 @@
+
+export default function Education(
+    {education,
+         onChange,
+          handleSave,
+          handleDelete,
+        }){
+
+
+    return(
+        <div  className="educational-experience">
+            school:
+            <input 
+            type="text"
+            name="school"
+            value={education.school}
+            onChange={onChange} />
+            degree:
+            <input 
+            type="text"
+            name="degree"
+            value={education.degree}
+            onChange={onChange} />
+            start date
+            <input 
+            type="text"
+            name="startDate"
+            value={education.startDate}
+            onChange={onChange} />
+            end date
+            <input 
+            type="text"
+            name="endDate"
+            value={education.endDate}
+            onChange={onChange} />
+            <button onClick={handleSave}>save</button>
+            <button onClick={handleDelete}>Delete</button>
+        </div>
+    )
+   
+    
+}
+
