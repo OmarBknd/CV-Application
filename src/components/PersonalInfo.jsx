b47c88ab2886378dd4e765bf21a3d1cd40c56747
OmarBknd <omar.bknd@gmail.com>
2024-11-01 13:52:30 +0100

    added location input

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,10 +1,28 @@
 export default function PersonalInfo({personalInfo,onChange}){
     return(
         <div>
-            
+            Name
             <input 
             name="name"
             value={personalInfo.name}
+            onChange={onChange}
+            type="text" />
+            Email
+             <input 
+            name="email"
+            value={personalInfo.email}
+            onChange={onChange}
+            type="text" />
+            Phone number
+             <input 
+            name="phoneNumber"
+            value={personalInfo.phoneNumber}
+            onChange={onChange}
+            type="text" />
+             Location(city,state)
+             <input 
+            name="phoneNumber"
+            value={personalInfo.location}
             onChange={onChange}
             type="text" />
         </div>
